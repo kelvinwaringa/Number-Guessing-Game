3ba68ddcbc848b9786f1e83bbeae2ff6cde7cf03
Kelvin Waringa <waringakelvin4@gmail.com>
2024-09-04 11:01:14 +0300

    Add initial project files for number guessing game
    - Added HTML, CSS, and JavaScript files
    - Added basic styling and game logic

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please enter a number!';
+    return;
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // Update highscore if current score is higher
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //When number is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector(
+        '.message'
+      ).textContent = `Game Over! The correct number was ${secretNumber}.`;
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //When number is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector(
+        '.message'
+      ).textContent = `Game Over! The correct number was ${secretNumber}.`;
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Game Reset Functionality
+document.querySelector('.again').addEventListener('click', function () {
+  // Reset the score and generate a new secret number
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Restore initial conditions of the message, number, and guess input field
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  // Restore the original background color and number width
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+});
